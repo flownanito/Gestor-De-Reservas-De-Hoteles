da5e2b0761103a5dbe42c4cab5eb7042bf6eb568
Reda Betancor <113508816+depraider@users.noreply.github.com>
2025-11-12 16:10:02 +0000

    Add files via upload

---
+++ GestorReservasHotel/src/main/java/com/proyect/reservationmanager/model/Client.java
@@ -0,0 +1,47 @@
+package com.proyect.reservationmanager.model;
+
+import jakarta.persistence.*; // Paquete para JPA (Hibernate)
+import jakarta.validation.constraints.*; // Para validaciones
+import lombok.*; // Para simplificar el código Java
+
+import java.time.LocalDateTime;
+
+@Entity // Indica que esta clase es una tabla de base de datos
+@Table(name = "Clients") // Especifica el nombre real de la tabla en la BD
+@Data // Lombok genera los metodos get/set/toString automaticamente
+@NoArgsConstructor // Lombok Constructor sin argumentos (obligatorio para JPA)
+@AllArgsConstructor // Lombok Constructor con todos los argumentos
+public class Client {
+  // PK: id_cliente
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // PostgreSQL usa IDENTIFY para auto-incremento
+  private Long clientId;
+
+  // dni (VARCHAR(15), UNIQUE, NOT NULL)
+  @Column(unique = true, nullable = false, length = 15)
+  @NotBlank(message = "DNI is mandatory")
+  private String dni;
+
+  // nombre (VARCHAR(100), NOT NULL)
+  @Column(nullable = false, length = 100)
+  @NotBlank(message = "First name is mandatory")
+  private String firstName;
+
+  // apellido (VARCHAR(100), NOT NULL)
+  @Column(nullable = false, length = 100)
+  @NotBlank(message = "Last name is mandatory")
+  private String lastName;
+
+  // email (VARCHAR(100), UNIQUE)
+  @Column(unique = true, length = 100)
+  @Email(message = "Email must be valid")
+  private String email;
+
+  // telefono (VARCHAR(20))
+  @Column(length = 20)
+  private String phone;
+
+  // fecha_registro (TIMESTAMP, NOT NULL)
+  @Column(nullable = false)
+  private LocalDateTime registrationDate;
+}
