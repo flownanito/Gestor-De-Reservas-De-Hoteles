1a4d8f942607a25d80b2d8c724b174baad4fe7f2
Naaynet Solutions <165549271+flownanito@users.noreply.github.com>
2025-12-02 23:31:14 +0000

    creda la pagina (reservationdetail) sino que hay un error con los estilos ya que no se muestran los estilos de esta pagina

--- GestorReservasHotel/frontend/src/App.jsx
+++ GestorReservasHotel/frontend/src/App.jsx
@@ -10,6 +10,9 @@ import ReservationsPage from './pages/ReservationsPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+// importacion de pruba de que se ve bien el reservation detail 
+import ReservationDetail from './pages/reservationdetail/ReservationDetail';
+
 export default function App() {
   return (
     <>
@@ -26,6 +29,7 @@ export default function App() {
           <Route path='/clients' element={<ClientsPage />} />
           <Route path='/employees' element={<EmployeesPage />} />
           <Route path='/reservations' element={<ReservationsPage />} />
+          <Route path='/reservationdetail' element={<ReservationDetail />} /> 
         </Routes>
       </main>
 
